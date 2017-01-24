d63a66b320cf8749f5c5c047b454937e75d260aa
Caleb Lemoine <circa10a@users.noreply.github.com>
2017-01-24 14:35:46 -0600

    Update notifications.js

--- js/notifications.js
+++ js/notifications.js
@@ -1,7 +1,7 @@
 var value = $('#circle').circleProgress('value');
 if (value >= 1.0){
   //$('#circle').circleProgress({ fill: "green" });
-  noty({text: '100% devices online', type: 'success'});
+  noty({text: '100% of devices online', type: 'success'});
 }
 else {
   noty({text: value * 100 + ' % of devices online', type: 'error'});
