5ac299068b3825bd118bc8bbdd527285aa8445db
circa10a <caleblemoine@gmail.comS>
2017-02-20 17:22:28 -0600

    notifications update

--- js/notifications.js
+++ js/notifications.js
@@ -1,8 +1,32 @@
 var value = $('#circle').circleProgress('value');
 if (value >= 1.0){
   //$('#circle').circleProgress({ fill: "green" });
-  noty({text: '100% of devices online', type: 'success'});
-}
-else {
-  noty({text: value * 100 + ' % of devices online', type: 'error'});
+  noty({text: '100% of devices online',
+  type: 'success',
+  theme: 'metroui',
+  closeWith   : ['click'],
+  progressBar : true,
+  timeout     : 10000,
+  animation   : {
+                    open  : 'animated bounceInRight',
+                    close : 'animated bounceOutRight',
+                    easing: 'swing',
+                 }
+
+  });
+
+} else {
+  noty({text: value * 100 + ' % of devices online',
+   type: 'error',
+   theme: 'metroui',
+   closeWith   : ['click'],
+   progressBar : true,
+   timeout     : 15000,
+   animation   : {
+                     open  : 'animated bounceInRight',
+                     close : 'animated bounceOutRight',
+                     easing: 'swing',
+                 }
+
+ });
 }
